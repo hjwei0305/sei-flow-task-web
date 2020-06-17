d5f69341abef76fdcfa6e6f294055863096e4cff
eason <cheng.yi@capitalcrux.com>
2020-06-17 10:19:04 +0800

    初始化

---
+++ config/router.config.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    path: '/user',
+    component: '../layouts/LoginLayout',
+    routes: [
+      { path: '/user', redirect: '/user/login' },
+      { path: '/user/login', component: './Login' },
+    ],
+  },
+  {
+    path: '/',
+    component: '../layouts/AuthLayout',
+    routes: [
+      { path: '/', redirect: '/dashboard' },
+      { path: '/dashboard', component: './Dashboard' },
+      {
+        path: '/task',
+        name: 'task',
+        routes: [{ path: '/task/home', component: './Task' }],
+      },
+    ],
+  },
+];
